a9db59dd53ea1b52cee6a0edbbba897cadb8a7c9
Filfeni <47361476+Filfeni@users.noreply.github.com>
2020-12-28 22:15:52 -0400

    Added Platform Specifics

--- Exercises/Exercises/App.xaml.cs
+++ Exercises/Exercises/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Exercises
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
